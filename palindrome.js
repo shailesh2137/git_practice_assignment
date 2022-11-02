11c841918c9d34670609cc5bfe61be2891f18de6
unknown <shaileshraut114397@gmail.com>
2022-11-02 23:13:58 +0530

    Add Palindrome & Prime

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+//Program To check Palindrome.
+
+let str="Naman";
+for( i=0 i<=str.length;i++)
+{
+	if(str[i]-1==str[j])
+	{
+		s="No";
+	}
+}
+console.log(bag);
---
+++ prime.js
@@ -0,0 +1,14 @@
+//Check Prime or Not
+
+let num=13;
+let bag="";
+for(let i=0;i<=num;i++)
+{
+	if(num%1==0)
+	{
+		bag++
+	}
+}
+if(bag==2)
+{
+	console.log(No);	
